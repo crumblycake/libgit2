2e2e4478e8cfbfc69ebcfba52d64b670d681d6d9
crumblycake <angusbjones@outlook.com>
2013-11-10 10:31:20 +1100

    Revert "Added recognition of SSH support for win32 and fixed up a python build gen script"
    This reverts commit 444d37ccad666d6d191cfbb62774c7b790653a69.

--- src/transport.c
+++ src/transport.c
@@ -80,13 +80,10 @@ static int transport_find_fn(const char *url, git_transport_cb *callback, void *
 	if (!definition && git_path_exists(url) && git_path_isdir(url))
 		definition = &local_transport_definition;
 
-	/* It could be a SSH remote path. Check to see if there's a : */
+	/* It could be a SSH remote path. Check to see if there's a :
+	 * SSH is an unsupported transport mechanism in this version of libgit2 */
 	if (!definition && strrchr(url, ':'))
-#ifdef GIT_SSH
-		definition = &ssh_transport_definition;
-#else
 		definition = &dummy_transport_definition;
-#endif
 #else
 	/* For other systems, perform the SSH check first, to avoid going to the
 	 * filesystem if it is not necessary */
